333ebba453fb9f4386cac900f0d8afa5e1a23715
Miguel Vázquez <miguelvz26.mv@gmail.com>
2022-11-22 19:30:52 -0600

    ejecutar seeders de nuevas tablas para calendario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
             ProductSeeder::class,
+            EventTypeSeeder::class,
+            ServiceTypeSeeder::class,
+            PackTypeSeeder::class,
         ]);
 
         User::create([
